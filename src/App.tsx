78b6dea08216ef3ddcc8144c1c840dc3fb8f09e0
71M-M414 <guilherme.vtm2@gmail.com>
2022-09-16 18:03:50 -0300

    Css e estrutura da página app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
+import styles from "./App.module.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+// interfaces
+import { useState } from "react";
+import { ITask } from "./interfaces/Task";
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer ? </h2>
+          <TaskForm
+            btnText="Criar tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList taskList={taskList} />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
