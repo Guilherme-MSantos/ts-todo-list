02448fad0967ac26e82535e47ed3d85fdbb7166b
71M-M414 <guilherme.vtm2@gmail.com>
2022-09-16 18:00:56 -0300

    Criação dos componentes header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        {" "}
+        <span>Guilherme Moreira</span> @ 2022
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.css";
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1> React + Ts todo</h1>
+    </header>
+  );
+};
+
+export default Header;
